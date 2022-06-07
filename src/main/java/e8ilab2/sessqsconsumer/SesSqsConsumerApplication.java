f612d6dc0e1d0a8c81a932d954ec8baba5963b7f
jessica.zanchetta <jessica.zanchetta@MacBook-Pro.local>
2022-06-07 12:22:37 -0300

    first-commit

---
+++ src/main/java/e8ilab2/sessqsconsumer/SesSqsConsumerApplication.java
@@ -0,0 +1,13 @@
+package e8ilab2.sessqsconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesSqsConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SesSqsConsumerApplication.class, args);
+	}
+
+}
